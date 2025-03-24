fda4ce402495b41fc2096b69380a210407773cf3
BalhegoDev <dribalhego@hotmail.com>
2025-03-24 16:54:26 -0300

    Rota get para pegar todos produtos de uma determinada empresa, completa

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -5,8 +5,14 @@ import { ParseIntPipe } from '@nestjs/common';
 @Controller('product')
 export class ProductController {
 
-   
+    constructor( private readonly productService:ProductService) {}
 
+    //essa rota irá retornar todos os produtos de uma loja específica
+    @Get("lojaProducts/:id")
+    getAllProducts(@Param("id", ParseIntPipe) id:number){
+        return this.productService.getAllLojaProducts(id)
+    }
 
+    
 
 }
